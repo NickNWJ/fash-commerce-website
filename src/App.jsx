4b6d43ccd5951244406541debd90b14319f9313a
NickNWJ <uarehup@gmail.com>
2022-03-05 11:30:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Home from "./pages/Home";
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+`;
+
+const nav = () => {
+  return <div>navbar</div>;
+};
+
+export default nav;
